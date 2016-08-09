4173b4e6793314de22f53b6b5ac8ea775a3a1f68
calebeggers <calebeggers11@gmail.com>
2016-08-09 12:56:45 -0400

    So many changes

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,8 @@ import 'angular-ui-router';
 import 'angular-cookies';
 
 import { config } from './utilities/config';
-import { SERVER } from './utilities/server';
+import { SERVER } from './utilities/servers/server';
+import { GITHUB } from './utilities/servers/github.server'
 
 import { HomeController }     from './controllers/home.controller';
 import { LayoutController }   from './controllers/layout.controller';
@@ -13,14 +14,20 @@ import { RegisterController } from './controllers/register.controller';
 
 // Import services
 import { LoginService } from './services/login.service';
+import { RegistrationService } from './services/registration.service'
+import { ProfileService } from './services/profile.service'
 
 angular
 	.module('app', ['ui.router', 'ngCookies'])
 	.config(config)
 	.constant('SERVER', SERVER)
+	.constant('GITHUB', GITHUB)
 	.controller('HomeController',     HomeController)
 	.controller('LayoutController',   LayoutController)
 	.controller('LoginController',    LoginController)
 	.controller('ProfileController',  ProfileController)
 	.controller('RegisterController', RegisterController)
 	.service('LoginService', LoginService)
+	.service('RegistrationService', RegistrationService)
+	.service('ProfileService', ProfileService)
+;
--- src/js/services/login.service.js
+++ src/js/services/login.service.js
@@ -1,15 +1,13 @@
 function LoginService ($http, SERVER, $cookies, $state) {
-
-this.login = login
-
-function login (user) {
-    $http.post(SERVER.URL + 'login', user).then( res => {
-      $cookies.put('access_token', res.data.access_token);
-      console.log("login service ran! : ", res)
-      // $state.go('profile'); ENDPOINT AFTER LOGIN IS TO BE DECIDED
-    });
-  }
+	this.login = login
+	function login (user) {
+		$http.post(SERVER.URL + 'login', user).then( res => {
+			$cookies.put('access_token', res.data.access_token);
+			console.log("login service ran! : ", res)
+			$state.go('root.profile', {username: res.data.github})
+		});
+	}
 }
 
 LoginService.$inject = ['$http', 'SERVER', '$cookies', '$state'];
-  export { LoginService };
+export { LoginService };
